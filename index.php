3212d102c802c21ef1cbb8c339066a718a5d38b7
abdelhalim <abdelhalimpro@gmail.com>
2022-01-11 21:41:04 +0100

    using the Request and the Response

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 
-// framework/index.php
-$name = $_GET['name'];
+require_once __DIR__.'/vendor/autoload.php';
 
-printf('Hello %s', $name);
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
+$name = $request->attributes->get('name', 'world');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+
+$response->send();
